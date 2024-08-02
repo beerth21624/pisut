3a744dc6ab3a543dae4c39098f03edd8d0ea94ba
beerth21624 <beerzii.4321@gmail.com>
2024-08-02 23:37:24 +0700

    update header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>pisut</title>
+        <meta name="description" content=""/>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
